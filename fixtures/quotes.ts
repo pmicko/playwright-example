2679d17d9fb4b06d0a96a90fafd98229f80b43b1
petrmi <petr.micko@gmail.com>
2023-05-23 15:41:00 +0200

    add playwright 🎭

---
+++ fixtures/quotes.ts
@@ -0,0 +1,17 @@
+export const QUOTES =
+{
+    "famous": [
+        "Yes there is a lot of people doing a great job out there.",
+        "If your life was a horse, you'd have to shoot it.",
+        "It's not a bug - it's an undocumented feature.",
+        "A classic is something that everyone wants to have read and nobody wants to read.",
+        "You have taken yourself too seriously."
+    ],
+    "awesome": [
+        "Do something unusual today. Pay a bill.",
+        "Excellent time to become a missing person.",
+        "I love deadlines. I love the whooshing sound they make as they fly by.",
+        "Nothing so needs reforming as other people's habits.",
+        "Beware of low-flying butterflies."
+    ]
+}
---
+++ tests/qa-homework-example.spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from "@playwright/test";
+import { QUOTES } from "../fixtures/quotes";
+
+test('Test quotes as a user flow', async({ page }) => {
+    // Start 💡
+    await page.goto('/')
+    await expect(page).toHaveTitle(/QA Engineer/);
+
+    // Main Page 💡
+    await page.getByText('Enter').click()
+    await expect(page).toHaveURL(/.*code/);
+
+    // Code Page 💡
+    const secretCode = await page.getAttribute('[name=secret]', 'value')
+    await page.locator('#code').fill(secretCode)
+
+    await page.getByLabel('Are you a robot? ').click()
+    await expect(page.getByLabel('Are you a robot?')).toBeChecked()
+
+    await page.getByRole('button', { name: 'Submit' }).click()
+    await expect(page).toHaveURL(/lists/);
+
+    // Lists Page 💡
+    const categories = await page.locator('h2').allTextContents()
+    expect(categories.sort()).toEqual(['Awesome quotes', 'Famous quotes'])
+
+    const awesomeQuotesFound = await page.locator('[qa-id=Awesome] span:not(.score)').allTextContents()
+    expect(awesomeQuotesFound.sort()).toEqual(QUOTES.awesome.sort())
+
+    const famousQuotesFound = await page.locator('[qa-id=Famous] span:not(.score)').allTextContents()
+    expect(famousQuotesFound.sort()).toEqual(QUOTES.famous.sort())
+
+    const individualScores = await page.locator('.score').allTextContents()
+    let sumOfScores = 0;
+    individualScores.map( score => sumOfScores += Number(score))
+
+    const pageSummary = await page.locator('#summary').textContent()
+    const pageTotalScore = Number(pageSummary.split('Total score: ')[1]);
+
+    expect(pageTotalScore).toEqual(sumOfScores)
+})
